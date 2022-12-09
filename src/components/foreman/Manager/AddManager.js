c2380074f6da05a04ce7dd7a4b52d239539d2a87
Sarath Kumar S <sarath.s@experionglobal.com>
2022-12-09 12:50:12 +0530

    conflict resolved

--- src/components/foreman/Manager/AddManager.js
+++ src/components/foreman/Manager/AddManager.js
@@ -1,120 +1,46 @@
 import { ClassNames } from '@emotion/react';
 import React, { useState } from 'react';
 import classes from './AddManager.module.css';
-import Axios from 'axios';
+import axios from 'axios';
 
 const AddManager=()=>{
-	const url = "http://localhost:8080/api/addmanager"
+	const[id,setId]=useState("");
+	const[fname,setFname]=useState("");
+	const[lname,setLname]=useState("");
+	const[email,setEmail]=useState("");
 
-const [data,setData] = useState({
-	emp_id:"",
-	firstName:"",
-	emp_lastname:"",
-	email:"",
-	mobileNumber:"",
-	passWord:"manager@123"
-})
-
-function handle(e){
-	const newdata = {...data}
-	newdata[e.target.id] = e.target.value
-	setData(newdata)
-	console.log(newdata)
-  }
-  
-  function submit(e){
-	e.preventDefault();
-	Axios.post(url,{
-		emp_id:parseInt(data.emp_id),
-		firstName:data.firstName,
-		emp_lastname:data.emp_lastname,
-		email:data.email,
-		mobileNumber:parseInt(data.mobileNumber),
-		passWord:data.passWord
-	})
-	.then(res=>{
-	  if(res.data != null){
-		alert("Manager added successfully")
-	  }
-	  console.log(res.data)
-	})
-  }
-  
+	let handleSubmit=async(e)=>{
+		e.preventDefault();
+		try{
+			axios.post("http://localhost:8080/api/managersadd",{
+			emp_id:id,
+			firstName:fname,
+			emp_lastname:lname,
+			email:email,
+		});
+	// let resJson=await res.json();
+	
+	setId("");
+	setFname("");
+	setLname("");
+	setEmail("");
+	
+	}catch(err){
+		console.log(err);
+	}
+	}
 return(
     <div className={classes.manage}>
         <div className={classes.upload}>
             <div className={classes.filedata}>
-			<form onSubmit={(e)=>submit(e)}>
-			<div className="form-group mt-3">
-              <label>Employee Id</label>
-              <span class="required">*</span>
-              <input
-                onChange={(e)=>handle(e)}
-                id="emp_id"
-                value={data.emp_id}
-                type="text"
-                className="form-control mt-1"
-                required
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>First Name</label>
-              <span class="required">*</span>
-              <input
-                onChange={(e)=>handle(e)}
-                id="firstName"
-                value={data.firstName}
-                type="name"
-                className="form-control mt-1"
-                placeholder="e.g Jane "
-                required
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Last Name</label>
-              <span class="required">*</span>
-              <input
-				onChange={(e)=>handle(e)}
-				id="emp_lastname"
-				value={data.emp_lastname}
-                type="name"
-                className="form-control mt-1"
-                placeholder="e.g Doe"
-                required
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Email</label>
-              <span class="required">*</span>
-              <input
-				onChange={(e)=>handle(e)}
-				id="email"
-				value={data.email}
-                type="email"
-                className="form-control mt-1"
-                placeholder="e.g abc@gmail.com"
-              required/>
-            </div>
-            <div className="form-group mt-3">
-              <label>Mobile No.</label>
-              <span class="required">*</span>
-              <input
-              onChange={(e)=>handle(e)}
-              id="mobileNumber"
-              value={data.mobileNumber}
-                type="text"
-                className="form-control mt-1"
-                placeholder="Mobile No."
-                required
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit">
-                Add
-              </button>
-            </div>
-         
-        </form>
+				<form onSubmit={handleSubmit}>
+					<input type="text" value={id} placeholder="Employee ID" onChange={(e)=>setId(e.target.value)}/>
+					<input type="text" value={fname} placeholder="First Name" onChange={(e)=>setFname(e.target.value)}/>
+					<input type="text" value={lname} placeholder="Last Name" onChange={(e)=>setLname(e.target.value)}/>
+					<input type="text" value={email} placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/>
+
+                    <div><button type="submit">Add</button></div>
+                </form>
             </div>
         </div>
 	</div>
