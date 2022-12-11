c488f50e14979121f83199f04ebb4531b1d78039
Sarath Kumar S <sarath.s@experionglobal.com>
2022-12-11 13:59:00 +0530

    details of assigned chits in manager

--- src/components/Manager/pages/StartedChits/StartedChits.js
+++ src/components/Manager/pages/StartedChits/StartedChits.js
@@ -6,20 +6,7 @@ const StartChit = (props) => {
     return (
         <Fragment>
             <Navbar />
-            <ul>
-                {props.chits.map((chit) => (
-                    <Chit
-                    id = {chit.id}
-                    chitNumber = {chit.chitNumber}
-                    installment = {chit.installment}
-                    duration = {chit.duration}
-                    numberOfChittal = {chit.numberOfChittal}
-                    currentNumberOfChittal = {chit.currentNumberOfChittal}
-                    totalAmount = {chit.totalAmount}
-                    launchDate = {chit.launchDate}
-                    />
-                ))}
-            </ul>
+           
         </Fragment>
     )
 }
