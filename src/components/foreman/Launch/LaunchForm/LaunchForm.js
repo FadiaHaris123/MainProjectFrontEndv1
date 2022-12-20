8bf79898bade0511a9f8f427e6399081c592e535
FadiaHaris123 <fadia.haris@experionglobal.com>
2022-12-20 13:02:43 +0530

    auction room manager edits

--- src/components/foreman/Launch/LaunchForm/LaunchForm.js
+++ src/components/foreman/Launch/LaunchForm/LaunchForm.js
@@ -41,6 +41,7 @@ const LaunchForm = () => {
     .then(res=>{
       if(res.data != null){
       alert("Chitty launched successfully")
+      form.reset();
       }
       console.log(res.data)
     })
